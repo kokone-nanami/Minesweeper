ab8293dcf5de59a5ea6a65258e88e548e32f833e
alicenychto <irol2273@icloud.com>
2020-06-06 16:16:50 +0200

    stage 5

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -6,7 +6,7 @@ import minesweeper.Status.*
 
 enum class GameStatus { ONGOING, LOST, WIN }
 
-enum class Status { NUMBER, BACK, FLAG }
+enum class Status { FRONT, BACK, FLAG }
 
 val around: Array<Pair<Int, Int>> = arrayOf(
     -1 to -1, -1 to 0, -1 to 1, 0 to -1, 0 to 0, 0 to 1, 1 to -1, 1 to 0, 1 to 1
@@ -25,6 +25,11 @@ class MineSweeper(val size: Int, val count: Int) {
 
     var gameStatus = ONGOING
 
+    constructor(size: Int, count: Int, marked: MutableSet<Pair<Int, Int>>): this(size, count) {
+        for (x in marked) {
+            field[x.first][x.second].status = FLAG
+        }
+    }
 
     init {
         var cnt = count
@@ -44,73 +49,158 @@ class MineSweeper(val size: Int, val count: Int) {
                             val col = j + offset.second
                             if (row >= 0 && col >= 0 && row < size && col < size && field[row][col].value != 9) {
                                 field[row][col].value++
-                                field[row][col].status = NUMBER
                             }
                         }
                     }
                 }
     }
 
-    fun showMap() {
-        println(" │123456789│")
-        println("—│—————————│")
-        for (i in field.indices) {
-            print("${i+1}│")
-            field[i].map { y ->
-                if (y.status == NUMBER)
-                    print(y.value)
-                else if (y.status == FLAG)
-                    print('*')
-                else
-                    print('.')
+    private fun bloom(x: Int, y: Int) {
+        val SAFE_CELL = 0
+        if (field[x][y].value != SAFE_CELL)
+            return
+        around.forEach { offset ->
+            run {
+                val row = x + offset.first
+                val col = y + offset.second
+                if (row >= 0 && col >= 0 && row < size && col < size && (field[row][col].status == BACK || field[row][col].status == FLAG)) {
+                    field[row][col].status = FRONT
+                    bloom(row, col)
+                }
             }
-            println("│")
         }
-        println("—│—————————│")
     }
 
-    fun toggleCase(x: Int, y: Int): Boolean {
-        if (field[x][y].status == NUMBER) {
-            return false
+    fun showMap() {
+        if (gameStatus == ONGOING) {
+            println(" │123456789│")
+            println("—│—————————│")
+            for (i in field.indices) {
+                print("${i + 1}│")
+                field[i].map { y ->
+                    when (y.status) {
+                        FRONT -> if (y.value == 0) print("/") else if (y.value == 9) print('X') else print(y.value)
+                        FLAG -> print('*')
+                        BACK -> print('.')
+                    }
+                }
+                println("│")
+            }
+            println("—│—————————│")
         }
         else {
-            if (field[x][y].status == BACK) {
-                field[x][y].status = FLAG
+            println(" │123456789│")
+            println("—│—————————│")
+            for (i in field.indices) {
+                print("${i + 1}│")
+                field[i].map { y ->
+                    if (y.status == FLAG && y.value != 9) {
+                        if (y.value == 0)
+                            print('/')
+                        else
+                            print(y.value)
+                    }
+                    else if (y.status == FLAG)
+                        print('X')
+                    else if (y.status == BACK && y.value == 9)
+                        print('X')
+                    else if (y.status == BACK)
+                        print('.')
+                    else if (y.value == 0)
+                        print("/")
+                    else if (y.value == 9)
+                        print('X')
+                    else
+                        print(y.value)
+                }
+                println("│")
             }
-            else {
-                field[x][y].status = BACK
-            }
-            return true
+            println("—│—————————│")
         }
+    }
+
+    fun claim(x: Int, y: Int) {
+        if (x >= 0 && y >= 0 && x < size && y < size) {
+            field[x][y].status = FRONT
+            if (field[x][y].value == 9) {
+                gameStatus = LOST
+                return
+            }
+            bloom(x, y)
+        }
+    }
+
+    fun mark(x: Int, y: Int) {
+        if (field[x][y].status == BACK) {
+            field[x][y].status = FLAG
+        }
+        else {
+            field[x][y].status = BACK
+        }
+    }
+
+    fun lost(): Boolean {
+        return gameStatus == LOST
     }
 
     fun win(): Boolean {
         var remain = 0
-        var wrong = 0
         for (i in 0 until size)
-            for (j in 0 until size) {
-                if (field[i][j].status == BACK && field[i][j].value == 9)
+            for (j in 0 until size)
+                if (field[i][j].status == BACK || field[i][j].status == FLAG)
                     remain++
-                if (field[i][j].status == FLAG && field[i][j].value != 9)
-                    wrong++
-            }
-        if (remain != 0 || wrong != 0) return false
-        else return gameStatus != LOST
+        if (remain != count) return false
+        else if (gameStatus != LOST) {
+            gameStatus = WIN
+            return true
+        }
+        else
+            return false
     }
 
 }
 
 fun main() {
     val s = askForInput()
-    val ms = MineSweeper(9, s)
+    var ms = MineSweeper(9, s)
+    var explored = false
+    val markedList = mutableSetOf<Pair<Int, Int>>()
     ms.showMap()
-    while (!ms.win()) {
+    outer@ while (true) {
         println("Set/delete mines marks (x and y coordinates): ")
-        val coo = readLine()!!.split(" ").map { it.toInt() }
-        if (ms.toggleCase(coo[1]-1, coo[0]-1))
+        val cmd = readLine()!!.split(" ")
+        if (cmd[2] == "mine") {
+            val x = cmd[1].toInt() - 1
+            val y = cmd[0].toInt() - 1
+            ms.mark(x, y)
+            if (!explored) {
+                if (markedList.contains(x to y))
+                    markedList -= x to y
+                else
+                    markedList += x to y
+            }
+        }
+        if (cmd[2] == "free") {
+            val x = cmd[1].toInt() - 1
+            val y = cmd[0].toInt() - 1
+            if (!explored) {
+                while (ms.field[x][y].value == 9) {
+                    ms = MineSweeper(9, s, markedList)
+                }
+            }
+            ms.claim(cmd[1].toInt() - 1, cmd[0].toInt() - 1)
+            explored = true
+        }
+        if (ms.win()) {
             ms.showMap()
-        else
-            println("There is a number here!")
+            println("Congratulations! You found all mines!")
+            break@outer
+        }
+        if (ms.lost()) {
+            ms.showMap()
+            println("You stepped on a mine and failed!")
+            break@outer
+        }
+        ms.showMap()
     }
-    println("Congratulations! You found all mines!")
 }
